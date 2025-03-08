28998821dd69a1aeb5143319d236dcac20f3eb92
Digg¢ <70993871+digger1213@users.noreply.github.com>
2025-03-08 14:43:29 +1300

    Fix windshield 'copying' the light interactable

--- source/Patches/PlayerController.cs
+++ source/Patches/PlayerController.cs
@@ -3,9 +3,7 @@ using CruiserImproved.Utils;
 using GameNetcodeStuff;
 using HarmonyLib;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection.Emit;
-using System.Reflection;
 using UnityEngine;
 
 namespace CruiserImproved.Patches;
@@ -65,5 +63,54 @@ internal class PlayerControllerPatches
             var rb = scanNode.gameObject.AddComponent<Rigidbody>();
             rb.isKinematic = true;
         }
+    }
+
+    [HarmonyPatch("SetHoverTipAndCurrentInteractTrigger")]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> SetHoverTipAndCurrentInteractTrigger_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        //Fix untagged interactables from carrying the last valid interactable the player looked at (caused the Cabin Light interact prompt to carry over the whole WindshieldInteractBlocker)
+        var codes = new List<CodeInstruction>(instructions);
+
+        var get_layer = PatchUtils.Method(typeof(GameObject), "get_layer");
+
+        int insertIndex = PatchUtils.LocateCodeSegment(0, codes, [
+            new(OpCodes.Callvirt, get_layer),
+            new(OpCodes.Ldc_I4_S, 0x1E),
+            new(OpCodes.Beq),
+            ]);
+
+        if(insertIndex == -1)
+        {
+            CruiserImproved.LogWarning("Could not transpile SetHoverTipAndCurrentInteractTrigger!");
+            return codes;
+        }
+
+        var jumpDestination = codes[insertIndex + 2].operand;
+
+        insertIndex += 3; //after the searched sequence
+
+        var insertMethod = PatchUtils.Method(typeof(PlayerControllerPatches), nameof(ValidRayHit));
+
+        /*
+         *  IL Code (adding new condition to the end of the existing if statement beginning with Physics.Raycast):
+         *  
+         *  && PlayerControllerPatches.ValidRayHit(this)
+         */
+
+        codes.InsertRange(insertIndex, [
+            new(OpCodes.Ldarg_0),
+            new(OpCodes.Call, insertMethod),
+            new(OpCodes.Brfalse, jumpDestination)
+            ]);
+
+        return codes;
+    }
+
+    static bool ValidRayHit(PlayerControllerB player)
+    {
+        //Return true if the looked at object is a valid interactable
+        string tag = player.hit.collider.tag;
+        return tag == "PhysicsProp" || tag == "InteractTrigger";
     }
 }
